9416472b4149f494f734ba0844dc9234e0aba7f2
Joaquim Perez <jquim1@hotmail.com>
2021-10-03 16:02:32 +0100

    user info cache added

--- handlers/APIHandler.py
+++ handlers/APIHandler.py
@@ -5,6 +5,8 @@ from .IGHandler import IGHandler
 
 api = Blueprint( "APIHandler", __name__ )
 
+__user_info_cache = { }
+
 class APIHandler( ):
 
 	def get_blueprint( self ):
@@ -28,4 +30,10 @@ class APIHandler( ):
 
 	@api.route( '/get-user-info/<string:user_name>', methods = [ 'GET' ] )
 	def get_user_info( user_name ):
-		return IGHandler.get_user_info( user_name )
+                user_info = { }
+                if __user_info_cache.has_key( user_name ):
+                        user_info = __user_info_cache[ user_name ]
+                else
+                        user_info = IGHandler.get_user_info( user_name )
+                        __user_info_cache[ user_name ] = user_info
+		return user_info
