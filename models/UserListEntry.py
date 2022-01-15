add461936b7de1f5d7e24846ec52c78dc295b93f
Joaquim Perez <jquim1@hotmail.com>
2022-01-15 21:09:14 +0000

    id fix

--- models/UserListEntry.py
+++ models/UserListEntry.py
@@ -3,7 +3,6 @@ from peewee import *
 from base.BaseModel import BaseModel
 
 class UserListEntry( BaseModel ):
-    id = AutoField( )
     owner_pk = IntegerField( )
     entry_pk = IntegerField( )
     ig_mode = TextField( )
