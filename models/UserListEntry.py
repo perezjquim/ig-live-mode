ba445acf8ccbb8374881bbe49346fe3fe408abc4
Joaquim Perez <jquim1@hotmail.com>
2022-01-15 18:10:49 +0000

    model fix

--- models/UserListEntry.py
+++ models/UserListEntry.py
@@ -3,7 +3,6 @@ from peewee import *
 from base.BaseModel import BaseModel
 
 class UserListEntry( BaseModel ):
-    id = AutoField( primary_key = True )
     owner_pk = IntegerField( )
     entry_pk = IntegerField( )
     ig_mode = TextField( )
