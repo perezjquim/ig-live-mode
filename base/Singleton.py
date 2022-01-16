3473bff3489e06199f3906b211d59bc856f5643c
Joaquim Perez <jquim1@hotmail.com>
2022-01-16 13:47:25 +0000

    typo fix

--- base/Singleton.py
+++ base/Singleton.py
@@ -1,8 +1,8 @@
 class Singleton( object ):
     _instance = None
 
-    def __new__(cls):
+    def __new__( cls ):
         if cls._instance is None:
-            print('Creating the object')
-            cls._instance = super(Logger, cls).__new__(cls)
+            print( '-- Creating the object --' )
+            cls._instance = super( Singleton, cls ).__new__( cls )
         return cls._instance
