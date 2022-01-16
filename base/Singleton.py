3c7f2559aa35a68ad8959daf18a863305955c91c
Joaquim Perez <jquim1@hotmail.com>
2022-01-16 13:50:26 +0000

    lock added in singleton

--- base/Singleton.py
+++ base/Singleton.py
@@ -1,8 +1,12 @@
+import threading
+
 class Singleton( object ):
+    _lock = threading.Lock( )
     _instance = None
 
     def __new__( cls ):
-        if cls._instance is None:
-            print( '-- Creating the object --' )
-            cls._instance = super( Singleton, cls ).__new__( cls )
-        return cls._instance
+    	with cls._lock:
+	        if cls._instance is None:
+			print( '-- Creating the object --' )
+			cls._instance = super( Singleton, cls ).__new__( cls )
+        	return cls._instance
