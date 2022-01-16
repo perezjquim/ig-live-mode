87fac3e20b25805e7853d90a037a24042a4d4eda
Joaquim Perez <jquim1@hotmail.com>
2022-01-16 13:56:07 +0000

    indent fix

--- base/Singleton.py
+++ base/Singleton.py
@@ -1,12 +1,12 @@
 import threading
 
 class Singleton( object ):
-    _lock = threading.Lock( )
-    _instance = None
+	_lock = threading.Lock( )
+	_instance = None
 
-    def __new__( cls ):
-    	with cls._lock:
-	        if cls._instance is None:
-			print( '-- Creating the object --' )
-			cls._instance = super( Singleton, cls ).__new__( cls )
-        	return cls._instance
+	def __new__( cls ):
+		with cls._lock:
+			if cls._instance is None:
+				print( '-- Creating the object --' )
+				cls._instance = super( Singleton, cls ).__new__( cls )
+			return cls._instance
