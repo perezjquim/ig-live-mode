dd81a98fea0982847bf4c87a6ce689f5423906d2
Joaquim Perez <jquim1@hotmail.com>
2022-01-16 13:09:15 +0000

    singleton tweak

--- base/SingletonMetaClass.py
+++ base/SingletonMetaClass.py
@@ -1,12 +1,10 @@
 import threading
 
 class SingletonMetaClass( type ):
-	___lock = threading.Lock()
-	___instances = {}
+	___instances = { }
 
-	def __call__(cls, *args, **kwargs):
+	def __call__( cls, *args, **kwargs ):
 		if cls not in cls.___instances:
-	    		with cls.___lock:
-	        		if cls not in cls.___instances:
-            				cls.___instances[cls] = super(SingletonMetaClass, cls).__call__(*args, **kwargs)
+			print( '-- Creating new instance for Singleton! --' )
+			cls.___instances[cls] = super( SingletonMetaClass, cls ).__call__( *args, **kwargs )
 		return cls.___instances[cls]
